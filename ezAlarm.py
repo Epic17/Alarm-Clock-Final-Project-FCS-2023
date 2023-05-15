e9edef2f133ae1a38540c643fa5266179d983a3c
Epic17 <130080473+Epic17@users.noreply.github.com>
2023-05-15 11:33:38 -0500

    Output device & no entry checks work now
    -Output device check works and will not run app if no output device is found
    -No entry check works now and will not allow the user to not input anything
    -Compiled code into exe version 1.2.0
    -Removed old comments
    -Added comments describing functions

--- ezAlarm.py
+++ ezAlarm.py
@@ -1,6 +1,3 @@
-# Idea: add a feature that allows the user to title their alarm
-# If more time was available for this project, I would consider adding the ability to create multiple alarms.
-
 import time
 import datetime
 import winsound
@@ -12,43 +9,38 @@ hourentry = " Hour  "
 minuteentry = "Minute"
 periodentry = "Period"
 
+
+# This function enters the time into the "alarm" when the button is pressed
 def press(button):
     if button == "Cancel":
         app.stop()
     else:
         try:
-            p.get_default_output_device_info()
-        except IOError:
-            app.warningBox("Error", "No output device found. Please check your output device and try again.")
+            hr1 = app.getEntry(hourentry)
+            min1 = app.getEntry(minuteentry)
         
-        
-        hr1 = app.getEntry(hourentry)
-        min1 = app.getEntry(minuteentry)
-        
-        
-        #else:
-         #   app.warningBox("Error", "Please enter a valid value.")
-        
-        hr2 = round(hr1) # type: ignore
-        min2 = round(min1) # type: ignore
-        
-        if 13 > hr2 > 0:
-            global fin_hr
-            fin_hr = hr2
-            if 60 > min2 >= 0:
-                if 10 > min2:
-                    global fin_min
-                    fin_min = f"0{min2}"
-                    period_check()
+            hr2 = round(hr1) # type: ignore
+            min2 = round(min1) # type: ignore
+               
+            if 13 > hr2 > 0:
+                global fin_hr
+                fin_hr = hr2
+                if 60 > min2 >= 0:
+                    if 10 > min2:
+                        global fin_min
+                        fin_min = f"0{min2}"
+                        period_check()
+                    else:
+                        fin_min = min2
+                        period_check()
                 else:
-                    fin_min = min2
-                    period_check()
+                    app.warningBox("Error", "Please enter a minute between 0 and 59.")
             else:
-                app.warningBox("Error", "Please enter a minute between 0 and 59.")
-        else:
-            app.warningBox("Error", "Please enter a hour between 1 and 12.")
+                app.warningBox("Error", "Please enter a hour between 1 and 12.")
+        except TypeError:
+            app.warningBox("Error", "Please enter a valid value.")
 
-
+# This function makes sure the time is in the correct format
 def period_check():
     am_pm1 = app.getOptionBox(periodentry)
     global fin_hr
@@ -74,8 +66,8 @@ def period_check():
         else:
             alarm_time = f"{fin_hr}:{fin_min}:00"
             alarm(alarm_time)
-      
-            
+
+# This function is used for the alarm. It sets an alarm and plays the alarm sound as well     
 def alarm(set_alarm_timer):
     print("Your alarm has been set for:", set_alarm_timer)
     while True:
@@ -85,11 +77,14 @@ def alarm(set_alarm_timer):
         print(f"Current time: {current_time}")
         if current_time == set_alarm_timer:
             print("Time to wake up!")
-            for i in range(3):
-                winsound.PlaySound("AlarmSound.wav", winsound.SND_FILENAME)
+            try:
+                for i in range(3):
+                    winsound.PlaySound("AlarmSound.wav", winsound.SND_FILENAME)
+            except:
+                app.warningBox("Error", "Sound was unable to play. Please check your output device and try again.")
             break
 
-
+# Creates the application GUI
 app = gui("ezAlarm", "400x200")
 app.addLabel("title", "Set your alarm below.")
 app.setBg("lightblue")
@@ -102,5 +97,10 @@ app.addButtons(["Submit", "Cancel"], press)
 app.setFocus(hourentry)
 app.icon = "ezAlarm-icon.ico"
 
-
-app.go()
+# Checks for an output device. If found, continue with app startup.
+# If not, cancel app startup and prompt user to connect an output device.
+try:
+    p.get_default_output_device_info()
+    app.go()
+except IOError:
+    app.warningBox("Error", "No output device found. Please check your output device and try again.")
